b6dfc4bf50c34fe6b43dd6bd2ea3235959e36365
Diogo Koshiba <diogo.koshiba@gmail.com>
2016-09-19 20:47:25 -0300

    init + search + partialview

---
+++ Fiap.DesenvolvimentoWeb.Ajax/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Fiap.DesenvolvimentoWeb.Ajax.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.DesenvolvimentoWeb.Ajax.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult MostrarProduto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string BuscarProduto(int query)
+        {
+            return Db.BuscarProduto(query);
+        }
+
+        [HttpGet]
+        public ActionResult PesquisarProdutos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PesquisarProdutos(string query)
+        {
+            List<Product> lista = Db.ListarProdutos(query);
+
+            if (Request.IsAjaxRequest())
+                return PartialView("_ListaProdutos", lista);
+
+            return View(lista);
+        }
+    }
+}
---
+++ Fiap.DesenvolvimentoWeb.Ajax/DAL/Db.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.DesenvolvimentoWeb.Ajax.DAL
+{
+    public class Db
+    {
+        public static string BuscarProduto(int query)
+        {
+            using (var ctx = new AdventureWorksLT2008Entities())
+            {
+                try
+                {
+                    return ctx.Product.Where(p => p.ProductID == query).FirstOrDefault<Product>().Name;
+                }
+                catch
+                {
+                    return "nenhum produto com código " + query;
+                }
+            }
+        }
+
+        public static List<Product> ListarProdutos(string query)
+        {
+            using (var ctx = new AdventureWorksLT2008Entities())
+            {
+                if (string.IsNullOrEmpty(query))
+                    return ctx.Product.ToList<Product>();
+
+                return ctx.Product.Where(p => p.Name.StartsWith(query)).ToList<Product>();
+            }
+        }
+    }
+}
